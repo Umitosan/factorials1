b56960e45d453e36eaf77c35352b453779ef038f
Dominic Brown and Marko Ludlow <marko@>
2017-03-28 15:27:19 -0700

    add recursive version of javascript calculation code

--- js/scripts.js
+++ js/scripts.js
@@ -2,36 +2,52 @@ $(document).ready(function() {
 
   $("#mainform").submit(function(event){
     event.preventDefault();
-
-    // For example, 5! = 5 * 4 * 3 * 2 * 1 = 120.
+    // factorial example, 5! = 5 * 4 * 3 * 2 * 1 = 120.
 
 
     var numberArray = [];
     var userInput = parseInt($("#input1").val());
     var total = 1;
 
-    if (userInput > 100) {
+    // check for starting value too high
+    if (userInput > 200) {
       alert("My CPU can't handle that size");
-      userInput = 1;
+      userInput = "-";
+      total = "-";
     }
 
+    // WITH RECURSION
+    var tmpTotal = 1;
     for (i = 1; i <= userInput; i++) {
-      numberArray.push(i);
+      tmpTotal = (tmpTotal * i);
+      //show current iteration
+      console.log("tmpTotal= " , i);
     }
 
-    for (j = 0; j < numberArray.length; j++) {
-      total *= numberArray[j];
-      console.log("totalinloop: " , total)
-    }
+    // NON RECURSIVE METHOD
+    // for (i = 1; i <= userInput; i++) {
+    //   numberArray.push(i);
+    // }
+    //
+    // for (j = 0; j < numberArray.length; j++) {
+    //   total *= numberArray[j];
+    //   console.log("totalinloop: " , total)
+    // }
 
-    console.log("total: " , total);
+    // console.log("total: " , total);
+   console.log("tmptotal: " , tmpTotal);
 
+    //show form
     $(".word-output").show();
 
+    // clear output fields
     $("#yourStartnum").text("");
     $("#fact").text("");
+
+    // output starting and calculated values
     $("#yourStartnum").append(userInput);
-    $("#fact").append(total);
+    // $("#fact").append(total);
+    $("#fact").append(tmpTotal);
 
   });
 
